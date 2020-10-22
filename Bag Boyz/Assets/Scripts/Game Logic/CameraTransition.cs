1facb2ca7c82016a8b5d78930e84e6183ae7055e
James Spagnolo <1020554@student.sae.edu.au>
2020-10-22 12:56:03 +1100

    Camera Setup
    + Added StateDrivenCamera which swaps between cameras based on a variable
    + Added CameraTransition script which sets up invisible walls to trigger camera transitions when player collides with them
    + Added Cam1 Cam2 Trigger Invisible Wall which swaps back and forth between Cam 1 and Cam 2 when player collides with it.
    + Added Bakery Background w/ Collider set up

---
+++ Bag Boyz/Assets/Scripts/Game Logic/CameraTransition.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTransition : MonoBehaviour
+{
+    public Animator cameraAnimator;
+
+    public int currentCamera;
+    public int cameraTransition;
+
+    private void Update()
+    {
+        if (currentCamera == 1)
+        {
+            cameraTransition = 2;
+        }
+
+        else if (currentCamera == 2)
+        {
+            cameraTransition = 1;
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            switch(cameraTransition)
+            {
+                
+                case 1: 
+                    cameraAnimator.SetInteger("Camera", 1);
+                    currentCamera = 1;
+                    break;
+                case 2: 
+                    cameraAnimator.SetInteger("Camera", 2);
+                    currentCamera = 2;
+                    break;
+                default: Debug.LogError("No Camera Selected"); break;
+            }
+        }
+    }
+
+    
+}
