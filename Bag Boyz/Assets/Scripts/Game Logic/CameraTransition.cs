52e44c9a737a45c7ea43a0a4b02a62d56280f1f5
James Spagnolo <1020554@student.sae.edu.au>
2020-10-28 19:51:07 +1100

    Preparing Larger Scene
    + Improved Camera Transition logic to be less buggy
    + Preparing third camera and checkout section
    + Increased size of scenes
    
    *Working on prefabs for the bakery section

--- Bag Boyz/Assets/Scripts/Game Logic/CameraTransition.cs
+++ Bag Boyz/Assets/Scripts/Game Logic/CameraTransition.cs
@@ -5,21 +5,45 @@ using UnityEngine;
 public class CameraTransition : MonoBehaviour
 {
     public Animator cameraAnimator;
+    public GameObject player;
 
+    public int cameraOne;
+    public int cameraTwo;
+
+    [System.Serializable]
+    public enum cameraTrigger
+    {
+        Cam1toCam2,
+        Cam2toCam1,
+        Cam2toCam3,
+        Cam3toCam2,
+    }
+
+    public cameraTrigger thisTrigger;
+
+    public int camTrigger;
     public int currentCamera;
     public int cameraTransition;
 
+    bool cameraMoving = false;
+
+
+
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        DetermineCameraTransition(thisTrigger);
+    }
+
+
     private void Update()
     {
-        if (currentCamera == 1)
-        {
-            cameraTransition = 2;
-        }
 
-        else if (currentCamera == 2)
-        {
-            cameraTransition = 1;
-        }
+        
+        CheckPlayerWithinBounds(thisTrigger, player.transform.position);
+
     }
 
 
@@ -27,21 +51,77 @@ public class CameraTransition : MonoBehaviour
     {
         if (collision.tag == "Player")
         {
-            switch(cameraTransition)
+            cameraMoving = true;
+
+            switch(cameraOne)
             {
                 
                 case 1: 
-                    cameraAnimator.SetInteger("Camera", 1);
-                    currentCamera = 1;
+                    cameraAnimator.SetInteger("Camera", cameraTwo);
+                    thisTrigger = cameraTrigger.Cam2toCam1;
+                    DetermineCameraTransition(thisTrigger);
                     break;
                 case 2: 
-                    cameraAnimator.SetInteger("Camera", 2);
-                    currentCamera = 2;
+                    cameraAnimator.SetInteger("Camera", cameraTwo);
+                    thisTrigger = cameraTrigger.Cam1toCam2;
+                    DetermineCameraTransition(thisTrigger);
                     break;
                 default: Debug.LogError("No Camera Selected"); break;
             }
         }
     }
 
+
+    public void DetermineCameraTransition(cameraTrigger cam)
+    {
+        int i = (int)cam;
+
+        switch(i)
+        {
+            case 0:
+                cameraOne = 1;
+                cameraTwo = 2;
+                break;
+            case 1:
+                cameraOne = 2;
+                cameraTwo = 1;
+                break;
+            default:
+                Debug.Log("Error");
+                break;
+        }
+    }
+
+    
+    public void CheckPlayerWithinBounds(cameraTrigger cam, Vector3 playerPosition)
+    {
+        int i = (int)cam;
+
+        switch(i)
+        {
+            case 0:
+                if(playerPosition.x >= this.transform.position.x)
+                {
+                    cameraAnimator.SetInteger("Camera", 2);
+                }
+                else
+                {
+                    cameraAnimator.SetInteger("Camera", 1);
+                }
+                break;
+            case 1:
+                if(playerPosition.x <= this.transform.position.x)
+                {
+                    cameraAnimator.SetInteger("Camera", 1);
+                }
+                else
+                {
+                    cameraAnimator.SetInteger("Camera", 2);
+                }
+                break;
+            default:
+                break;
+        }
+    }
     
 }
