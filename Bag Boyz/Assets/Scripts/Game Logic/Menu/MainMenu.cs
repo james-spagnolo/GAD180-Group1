1ccd2d2585db281efbf6ecd377ad73aa920389af
James Spagnolo <1020554@student.sae.edu.au>
2020-11-18 15:58:00 +1100

    Options Menu & Volume Control
    + Added Options Menu for Main Menu
    + Added buttons to navigate to and from options menu
    + Added Volume control slider
    + Added SetVolume script

--- Bag Boyz/Assets/Scripts/Game Logic/Menu/MainMenu.cs
+++ Bag Boyz/Assets/Scripts/Game Logic/Menu/MainMenu.cs
@@ -8,11 +8,17 @@ public class MainMenu : MonoBehaviour
 
     public AudioSource menuMusic;
 
+    public GameObject[] options;
+
 
     private void Start()
     {
         //Play Main Menu Theme
         menuMusic.Play();
+
+        options = GameObject.FindGameObjectsWithTag("Options");
+
+        HideOptions();
     }
 
     
@@ -20,5 +26,23 @@ public class MainMenu : MonoBehaviour
     public void LoadLevel(string level)
     {
         SceneManager.LoadScene(level);
+    }
+
+
+    public void DisplayOptions()
+    {
+        foreach (GameObject g in options)
+        {
+            g.SetActive(true);
+        }
+    }
+
+
+    public void HideOptions()
+    {
+        foreach (GameObject g in options)
+        {
+            g.SetActive(false);
+        }
     }
 }
