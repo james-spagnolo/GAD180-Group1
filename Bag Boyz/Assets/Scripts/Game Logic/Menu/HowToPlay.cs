bd1a85407b5216a0fc19571fe87a7d2b2982db89
James Spagnolo <1020554@student.sae.edu.au>
2020-11-26 12:58:45 +1100

    HowToPlay Section
    + Setup HowToPlay Section functionality
    * Need to construct tutorials for it

---
+++ Bag Boyz/Assets/Scripts/Game Logic/Menu/HowToPlay.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HowToPlay : MonoBehaviour
+{
+
+    public Sprite[] tutorialSprites;
+    private Image tutorialImage;
+
+    private int currentTutorial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTutorial = 0;
+
+        tutorialImage = this.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Image>();
+        tutorialImage.sprite = tutorialSprites[currentTutorial];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tutorialImage.sprite = tutorialSprites[currentTutorial];
+    }
+
+
+    public void NextTutorial()
+    {
+        if (currentTutorial < (tutorialSprites.Length - 1))
+        {
+            currentTutorial += 1;
+        }
+    }
+
+
+    public void PreviousTutorial()
+    {
+        if (currentTutorial > 0)
+        {
+            currentTutorial -= 1;
+        }
+        else
+        {
+            currentTutorial = 0;
+        }
+    }
+}
